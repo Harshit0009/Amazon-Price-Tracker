5134989542848d8f668b453ce97785b2f7df9e15
Harshit Tiwari <96289939+Harshit0009@users.noreply.github.com>
2022-09-17 13:38:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import lxml
+
+Headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+    'Accept-Language':"en-US,en;q=0.9",
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding":"gzip, deflate"
+    # "Cookie":"PHPSESSID=nt0jrn1l833gka7get59hgjjl2",
+}
+
+URL = "https://www.amazon.in/Google-Pixel-Black-128GB-Storage/dp/B08H8VZ6PV/ref=sr_1_1?keywords=google+pixel+4a+5g&qid=1663362442&sprefix=google+pixel+4a%2Caps%2C370&sr=8-1"
+response = requests.get(URL, headers=Headers)
+
+
+soup = BeautifulSoup(response.content, "lxml")
+print(soup.prettify())
+instant_price = soup.find(name="span", class_="a-price-whole")
+price = int(instant_price.getText())
+print(price)
+mylist = []
+for i in price:
+    if price[i] >= 48 and price[i] <= 57:
+
+my_price = 25000
+if price<=my_price:
+    print(mail)
